2867a5f540a0f50b25ae54e5bbaf943fe8acde72
Lucas Cascao <you@example.com>
2020-02-15 09:59:04 -0300

    Inicialiazando repositório

---
+++ server/src/main/java/br/com/cascao/realmofcard/domain/CardBean.java
@@ -0,0 +1,6 @@
+package br.com.cascao.realmofcard.domain;
+
+public class CardBean {
+    private String name;
+    private TypeBean type;
+}
---
+++ server/src/main/java/br/com/cascao/realmofcard/domain/TypeBean.java
@@ -0,0 +1,19 @@
+package br.com.cascao.realmofcard.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
